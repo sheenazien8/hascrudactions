80f6a65c19c26fdaec96d706a004155974fea601
sheenazien8 <sheenazien08@gmail.com>
2021-01-17 06:33:09 +0700

    add icon slot button

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -13,7 +13,15 @@
       </div>
       <div class="{{ $card ? 'card-footer' : '' }}">
         <div class="{{ $card ? '' : 'col-12' }}">
-          <x-components-button :title="__('hascrudactions::app.global.submit')" :submit-icon="true"/>
+          <x-components-button :title="__('hascrudactions::app.global.submit')">
+            <x-slot name="icon">
+              <span>
+                <svg style="width:15px; margin-right:2px; margin-bottom:4px;" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4" />
+                </svg>
+              </span>
+            </x-slot>
+          </x-components-button>
         </div>
       </div>
     </form>
