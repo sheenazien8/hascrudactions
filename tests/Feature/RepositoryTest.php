a0f0412a83cd4930e7094bb644192d6ea5f2d66f
sheenazien8 <sheenazien08@gmail.com>
2021-01-18 19:58:22 +0700

    CRUD repository test

--- tests/Feature/RepositoryTest.php
+++ tests/Feature/RepositoryTest.php
@@ -29,10 +29,95 @@ class RepositoryTest extends TestCase
     }
 
     /** @test */
-    public function can_get_all_data_with_repository()
+    public function can_get_paginate_data()
+    {
+        $testingRepository = app(TestingModelRepository::class)->paginate(request());
+
+        $this->assertEquals("Illuminate\Pagination\LengthAwarePaginator", get_class($testingRepository));
+
+        $this->assertArrayHasKey('last_page', $testingRepository->toArray());
+        $this->assertArrayHasKey('last_page_url', $testingRepository->toArray());
+        $this->assertArrayHasKey('next_page_url', $testingRepository->toArray());
+        $this->assertArrayHasKey('data', $testingRepository->toArray());
+    }
+
+    /** @test */
+    public function can_get_all_data()
     {
         $testingRepository = app(TestingModelRepository::class)->get(request());
 
         $this->assertCount($this->count, $testingRepository, 'Record Has a ' . $this->count);
+
+        app(TestingModelRepository::class)->query()->truncate();
+    }
+
+    /** @test */
+    public function can_create()
+    {
+        request()->merge([
+            'name' => 'Test',
+            'salary' => rand(0000, 9999),
+            'join_date' => now()->format('Y-m-d')
+        ]);
+        app(TestingModelRepository::class)->create(request());
+
+        $this->assertDatabaseHas('testing_tables', request()->all());
+    }
+
+    /** @test */
+    public function can_update()
+    {
+        request()->merge([
+            'name' => 'Test',
+            'salary' => rand(0000, 9999),
+            'join_date' => now()->format('Y-m-d')
+        ]);
+
+        $createdData = app(TestingModelRepository::class)->create(request());
+
+        request()->merge([
+            'name' => 'Test 2',
+            'salary' => rand(0000, 9999),
+            'join_date' => now()->format('Y-m-d')
+        ]);
+        $updatedData = app(TestingModelRepository::class)->create(request(), $createdData->id);
+
+        $this->assertEquals('Test 2', $updatedData->name);
+    }
+
+    /** @test */
+    public function can_destory()
+    {
+        $id = rand(0, $this->count);
+        $findData = app(TestingModelRepository::class)->delete($id);
+
+        $this->assertCount($this->count - 1, app(TestingModelRepository::class)->get(request()));
+
+        $this->assertTrue($findData);
+    }
+
+    /** @test */
+    public function can_bulk_destroy()
+    {
+        $data = app(TestingModelRepository::class)->get(request());
+
+        $random_id = range(1, rand(1, $this->count));
+
+        request()->merge([
+            'ids' => $random_id
+        ]);
+
+        app(TestingModelRepository::class)->bulkDestroy(request());
+
+        $deleted_data = app(TestingModelRepository::class)->get(request());
+
+
+        $count = $data->count();
+        $result  = $count - count($random_id);
+        if (count($random_id) > $count) {
+            $result  = count($random_id) - $count;
+        }
+
+        $this->assertCount($result, $deleted_data);
     }
 }
